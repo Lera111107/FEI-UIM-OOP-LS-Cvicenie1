c4aaec98fbf98c44dfb8ae89d254503b5b9b6f3e
Lera111107 <100447442+Lera111107@users.noreply.github.com>
2022-03-09 17:31:46 +0100

    Prvi program

--- PrviProgram.java
+++ PrviProgram.java
@@ -1,5 +1,14 @@
+import java.awt.datatransfer.StringSelection;
+import java.util.Scanner;
+
 public class PrviProgram {
     public static void main(String [] args){
-        System.out.println("Hello world takes two");
+        System.out.print("Unesi broj ");
+        Scanner moje = new Scanner(System.in);
+        String s = moje.nextLine();
+
+        int n = Integer.parseInt(s);
+        System.out.print("rezultat je ");
+        System.out.println(n*(n+1)/2);
     }
 }
